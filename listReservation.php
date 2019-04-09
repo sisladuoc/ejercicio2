fc108d6fb8a6eda1a63c3f095447596a668eea9c
cetecom <cetecom@LC08L1527>
2019-04-08 21:24:40 -0300

    Ejercicio 2 nuevo

---
+++ listReservation.php
@@ -0,0 +1,52 @@
+<?php
+    include('lib/constantes.php');
+?>
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>HOSTALRIOAMAZONAS RESERVATION ONLINE</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+        <div id="titulo">
+            <div id="imag"></div>
+            <h2>Mi sitio</h2>
+        </div>
+        <div id="menu"><?php include('menu.php'); ?></div>
+        <div id="contenido">
+            <h2>Titulo pagina</h2>
+            <p>
+                contenido
+            </p>
+        </div>
+        <footer>
+            Antonio Varas #666, Fono de contacto: 000000000
+        </footer>
+    </body>
+
+    <script>        
+        $("#reservation").on("click", function (event) {
+            if (($("#lsreservation").css("display") != "none")) {
+                $("#lsreservation").css("display", "none");
+            } else {
+                $("#lsreservation").show();
+                $("#lsreservation").css("display", "block");
+            }
+        });      
+
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
+</html>
---
+++ menu.php
@@ -0,0 +1,8 @@
+<div class="list-group">
+<a  class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> HOME</a>
+<a id="about" class="list-group-item" href="<?=URLBASE;?>aboutChile.php" data-toggle="tooltip" data-toggle="tooltip" data-placement="right" title="ABOUT CHILE"><i class="fa fa-usd fa-2x"></i> ABOUT CHILE </a>
+<a id="reservations" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="RESERVATION" ><i class="fa fa-sun-o fa-2x"></i> RESERVATIONS</a>
+<a id="reservation" class="list-group-item submenu" href="<?=URLBASE;?>reservation.php" data-toggle="tooltip" data-placement="right" title="RESERVATION" ><i class="fa fa-sun-o fa-2x"></i> RESERVATION</a>
+<a id="lsreservation" class="list-group-item submenu" href="<?=URLBASE;?>listReservation.php" data-toggle="tooltip" data-placement="right" title="LIST RESERVATION"><i class="fa fa-book fa-2x"></i> LIST RESERVATION</a>
+</div>
+      
